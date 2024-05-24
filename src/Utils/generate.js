f7bfcc0064a3d34333811a700d20ba546b989d58
fertioga <fertioga@gmail.com>
2024-05-24 09:58:43 -0300

    fix: phone(BR) and cellphone(BR) format

--- src/Utils/generate.js
+++ src/Utils/generate.js
@@ -109,6 +109,7 @@ async function getListLastName(){
 
 function generatePhoneNumber(format) {
     let phoneNumber = '';
+    const dddsBrasil = ["11", "12", "13", "14", "15", "16", "17", "18", "19", "21", "22", "24", "27", "28", "31", "32", "33", "34", "35", "37", "38", "41", "42", "43", "44", "45", "46", "47", "48", "49", "51", "53", "54", "55"];
     
     // Helper function to generate random number with specific length
     function randomNumber(length, maxNumber = 0) {
@@ -122,13 +123,13 @@ function generatePhoneNumber(format) {
     // Generate phone numbers based on the format
     switch (format) {
         case 'cellphoneBr':
-            phoneNumber = `(0${randomNumber(2, 28)}) 9${randomNumber(4)}-${randomNumber(4)}`;
+            phoneNumber = `(${dddsBrasil[Math.floor(Math.random() * dddsBrasil.length)]}) 9${randomNumber(4).toString().padStart(4, '0')}-${randomNumber(4).toString().padStart(4, '0')}`;
             break;
         case 'cellphoneUs':
             phoneNumber = `+1 (${randomNumber(3)}) ${randomNumber(3)}-${randomNumber(4)}`;
             break;
         case 'phoneBr':
-            phoneNumber = `(0${randomNumber(2, 28)}) ${randomNumber(4)}-${randomNumber(4)}`;
+            phoneNumber = `(${dddsBrasil[Math.floor(Math.random() * dddsBrasil.length)]}) ${randomNumber(4)}-${randomNumber(4)}`;
             break;
         case 'phoneUs':
             phoneNumber = `+1 (${randomNumber(3)}) ${randomNumber(3)}-${randomNumber(4)}`;
